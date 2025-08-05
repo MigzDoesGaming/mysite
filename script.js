cf72b69be7e00bf35c084fecf53e8b1cd773bfa2
thejuanverse <35053042+MigzDoesGaming@users.noreply.github.com>
2025-08-05 09:13:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,6 @@ for (let i = 0; i < totalColumns; i++) {
   const column = document.createElement("div");
   column.className = "tile-column";
 
-  // Offset every other column downward for staggered effect
-  if (i % 2 !== 0) {
-    column.style.marginTop = "50px"; // adjust to taste
-  }
-
   for (let j = 0; j < tilesPerColumn; j++) {
     const tile = document.createElement("div");
     tile.className = "tile";
