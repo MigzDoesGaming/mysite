f86d67479e7d6636d11357026a37e429d76d982d
thejuanverse <35053042+MigzDoesGaming@users.noreply.github.com>
2025-08-05 09:23:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const tilesPerColumn = 4;
 for (let i = 0; i < totalColumns; i++) {
   const column = document.createElement("div");
   column.className = "tile-column";
-
+  
   for (let j = 0; j < tilesPerColumn; j++) {
     const tile = document.createElement("div");
     tile.className = "tile";
